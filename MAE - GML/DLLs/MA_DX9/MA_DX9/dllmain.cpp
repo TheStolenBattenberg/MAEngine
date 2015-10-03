3ada5c1a841dad4cebd49267562fe459b2c95e9d
TSB <mentisanimae@gmail.com>
2015-10-03 12:51:16 +0100

    Added Shaders to DLL Backend and GML Frontend
    ShaderSet, ShaderReset, ShaderCreate, ShaderDelete

--- MAE - GML/DLLs/MA_DX9/MA_DX9/dllmain.cpp
+++ MAE - GML/DLLs/MA_DX9/MA_DX9/dllmain.cpp
@@ -9,3 +9,69 @@ DLLEXPORT double MADX9_Init(long pointer)
 	MARenderBackend.d3ddev = (LPDIRECT3DDEVICE9) pointer;
 	return 1;
 }
+
+DLLEXPORT double MADX9_ShaderCreateHLSL9(char* VSCode, char* PSCode)
+{
+	IDirect3DVertexShader9 *VShd;
+	IDirect3DPixelShader9 *PShd;
+
+	//Compile the Shader
+	if(MARenderBackend.HLSL9Compile(VSCode, PSCode, &VShd, &PShd) == 0) 
+	{ return 0; }
+
+	//Add shaders to container and return shader index;
+	MARenderBackend.VShader.push_back(VShd);
+	MARenderBackend.PShader.push_back(PShd);
+	return MARenderBackend.PShader.size()-1;
+}
+
+DLLEXPORT double MADX9_ShaderSetHLSL9(double index)
+{
+	HRESULT result;
+
+	//Store the current Fixed Vertex Format.
+	MARenderBackend.d3ddev->GetFVF(&MARenderBackend.stFVF);
+
+	int ShaderIndex = (int) index;
+
+	//Set the Vertex Shader
+	result = MARenderBackend.d3ddev->SetVertexShader(MARenderBackend.VShader[ShaderIndex]);
+	if(FAILED(result)) { return 0; }
+
+	//Set the Pixel Shader
+	result = MARenderBackend.d3ddev->SetPixelShader(MARenderBackend.PShader[ShaderIndex]);
+	if(FAILED(result)) { return 0; }
+
+	return 1;
+}
+
+DLLEXPORT double MADX9_ShaderResetHLSL9()
+{
+	HRESULT result;
+
+	//Reset the Vertex Shader.
+	result = MARenderBackend.d3ddev->SetVertexShader(NULL);
+	if(FAILED(result)) { return 0; }
+
+	//Reset the Pixel Shader.
+	result = MARenderBackend.d3ddev->SetPixelShader(NULL);
+	if(FAILED(result)) { return 0; }
+
+	MARenderBackend.d3ddev->SetFVF(MARenderBackend.stFVF);
+	return 1;
+}
+
+DLLEXPORT double MADX9_ShaderDestroyHLSL9(double index)
+{
+	int ShaderIndex = (int) index;
+
+	//Delete Vertex Shader
+	MARenderBackend.VShader[ShaderIndex]->Release();
+	MARenderBackend.VShader.erase(MARenderBackend.VShader.begin() + ShaderIndex);
+
+	//Delete Pixel Shader
+	MARenderBackend.PShader[ShaderIndex]->Release();
+	MARenderBackend.PShader.erase(MARenderBackend.PShader.begin() + ShaderIndex);
+
+	return 1;
+}
---
+++ MAE - GML/MAE.gmx/scripts/MAE_ShaderCreate.gml
@@ -0,0 +1,22 @@
+///MAE_ShaderCreate(vertex_shader_code, pixel_shader_code, type);
+
+switch(argument2)
+{
+    case SHADER_HLSL9:
+        show_debug_message("Compiling and Setting HLSL9 shader!");
+        
+        result = external_call(global.MADX9_ShaderCreateHLSL9, argument0, argument1);
+        if(result == 0)
+        {
+            show_debug_message("Failed to compile shader.");
+            return 0;
+        }
+        show_debug_message("Compiled Vertex & Pixel Shader ID: "+string(result));
+        return result;
+    break;
+    
+    default:
+        show_debug_message("Invalid Shader Type!");
+        return 0;
+    break;
+}
