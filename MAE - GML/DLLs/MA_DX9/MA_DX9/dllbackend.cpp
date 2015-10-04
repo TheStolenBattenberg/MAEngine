3cea6abe1859d909a367f1e6a367b2c3c82b18cb
Florian Preinfalk <preinfalkflorian@gmail.com>
2015-10-04 17:13:36 +0200

    Fixed crashes in MA_DX9.dll

--- MAE - GML/DLLs/MA_DX9/MA_DX9/dllbackend.cpp
+++ MAE - GML/DLLs/MA_DX9/MA_DX9/dllbackend.cpp
@@ -5,27 +5,38 @@ int MADLLBackend::HLSL9Compile(LPCSTR VertexShaderString, LPCSTR PixelShaderStri
 	LPD3DXBUFFER VSCode, PSCode;
 	LPD3DXCONSTANTTABLE ConstantTable;
 	HRESULT result;
-
+	__asm {int 03h}
 	//Attempt to compile Vertex shader
 	result = D3DXCompileShader(VertexShaderString, strlen(VertexShaderString), NULL, NULL, "main", "vs_3_0", D3DXSHADER_ENABLE_BACKWARDS_COMPATIBILITY, &VSCode, NULL, &ConstantTable);
 	if(FAILED(result))
-	{ VSCode->Release(); return 0; }
-
+	{
+		return 0;
+	}
+	
 	//Attempt to create vertex shader
 	result = d3ddev->CreateVertexShader((DWORD *) VSCode->GetBufferPointer(), VSOut);
 	if(FAILED(result))
-	{ VSCode->Release(); return 0; }
-
+	{
+		VSCode->Release();
+		return 0;
+	}
+	
 	//Atempt to compile Pixel Shader
 	result = D3DXCompileShader(PixelShaderString, strlen(PixelShaderString), NULL, NULL, "main", "vs_3_0", D3DXSHADER_ENABLE_BACKWARDS_COMPATIBILITY, &PSCode, NULL, &ConstantTable);
 	if(FAILED(result))
-	{ PSCode->Release(); return 0; }
-
+	{
+		VSCode->Release();
+		return 0;
+	}
+	
 	//Atempt to create Pixel Shader
 	result = d3ddev->CreatePixelShader((DWORD *) PSCode->GetBufferPointer(), PSOut);
 	if(FAILED(result))
-	{ PSCode->Release(); return 0; }
-
-
+	{
+		VSCode->Release();
+		PSCode->Release();
+		return 0;
+	}
+	
 	return 1;
 }
