e29e22ebe6973dec03c4077c02323019a87a02f7
Florian Preinfalk <preinfalkflorian@gmail.com>
2015-11-02 19:01:31 +0100

    Fixed shader functions
    Some destroy functions were added too

--- MAE - GML/DLLs/MA_DX9/MA_DX9/dllbackend.cpp
+++ MAE - GML/DLLs/MA_DX9/MA_DX9/dllbackend.cpp
@@ -2,41 +2,51 @@
 
 MADLLBackend marb;
 
-int MADLLBackend::HLSL9Compile(LPCSTR VertexShaderString, LPCSTR PixelShaderString, IDirect3DVertexShader9 ** VSOut, IDirect3DPixelShader9 ** PSOut)
+int MADLLBackend::HLSL9Compile(LPCSTR VertexShaderString, LPCSTR PixelShaderString, LPDIRECT3DVERTEXSHADER9* VSOut, LPDIRECT3DPIXELSHADER9* PSOut)
 {
-	LPD3DXBUFFER VSCode, PSCode;
+	LPD3DXBUFFER Code;
 	LPD3DXCONSTANTTABLE ConstantTable;
 	HRESULT result;
-	
+
+	LPD3DXBUFFER buf;
+
 	//Attempt to compile Vertex shader
-	result = D3DXCompileShader(VertexShaderString, strlen(VertexShaderString), NULL, NULL, "main", "vs_3_0", D3DXSHADER_ENABLE_BACKWARDS_COMPATIBILITY, &VSCode, NULL, &ConstantTable);
-	if(FAILED(result))
+	result = D3DXCompileShader(VertexShaderString, strlen(VertexShaderString), NULL, NULL, "main", "vs_3_0", D3DXSHADER_ENABLE_BACKWARDS_COMPATIBILITY, &Code, &buf, &ConstantTable);
+
+	if (FAILED(result))
 	{
-		return 0;
-	}
-	
-	//Attempt to create vertex shader
-	result = d3ddev->CreateVertexShader((DWORD *) VSCode->GetBufferPointer(), VSOut);
-	if(FAILED(result))
-	{
-		VSCode->Release();
+		MessageBoxA(0, (char*)buf->GetBufferPointer(), "Failed to compile shader", MB_OK);
+		buf->Release();
 		return 0;
 	}
 
+	//Attempt to create vertex shader
+	result = d3ddev->CreateVertexShader((DWORD *) Code->GetBufferPointer(), VSOut);
+	
+	Code->Release();
+
+	if (FAILED(result))
+		return 0;
+
 	//Atempt to compile Pixel Shader
-	result = D3DXCompileShader(PixelShaderString, strlen(PixelShaderString), NULL, NULL, "main", "vs_3_0", D3DXSHADER_ENABLE_BACKWARDS_COMPATIBILITY, &PSCode, NULL, &ConstantTable);
-	if(FAILED(result))
+	result = D3DXCompileShader(PixelShaderString, strlen(PixelShaderString), NULL, NULL, "main", "vs_3_0", D3DXSHADER_ENABLE_BACKWARDS_COMPATIBILITY, &Code, &buf, &ConstantTable);
+	
+	if (FAILED(result))
 	{
-		VSCode->Release();
+		MessageBoxA(0, (char*)buf->GetBufferPointer(), "Failed to compile shader", MB_OK);
+		buf->Release();
+		(*VSOut)->Release();
 		return 0;
 	}
-	
+
 	//Atempt to create Pixel Shader
-	result = d3ddev->CreatePixelShader((DWORD *) PSCode->GetBufferPointer(), PSOut);
-	if(FAILED(result))
+	result = d3ddev->CreatePixelShader((DWORD *)Code->GetBufferPointer(), PSOut);
+
+	Code->Release();
+	
+	if (FAILED(result))
 	{
-		VSCode->Release();
-		PSCode->Release();
+		(*VSOut)->Release();
 		return 0;
 	}
 	
---
+++ MAE - GML/MAE.gmx/scripts/MAE_ModelDestroyMD2.gml
@@ -0,0 +1,2 @@
+///MAE_ModelDestroyMD2(index)
+return external_call(global.MADX9_MD2Destroy, argument0);
