199cf87b1798872a92b39d937dd2f2f9764676f5
TickleForce <koderkk@gmail.com>
2015-11-03 10:20:46 -0500

    Added more physics shapes and init flag
    Former-commit-id: ea11617242a6eb636942ceb6f2fd1cd1c8e18663

--- MAE - GML/DLLs/MA_Bullet/MA_Bullet/MABullet_Shapes.cpp
+++ MAE - GML/DLLs/MA_Bullet/MA_Bullet/MABullet_Shapes.cpp
@@ -12,6 +12,42 @@ DLLEXPORT MAB_ShapeCreateSphere(double Radius)
 	return G.addShape(shape);
 }
 
+DLLEXPORT MAB_ShapeCreateCapsule(double Radius, double Height, double UpAxis)
+{
+	int axis = (int)UpAxis;
+	btCollisionShape* shape;
+	if (axis == 0) shape = new btCapsuleShapeX((btScalar)Radius, (btScalar)Height);
+	else if (axis == 1) shape = new btCapsuleShape((btScalar)Radius, (btScalar)Height);
+	else shape = new btCapsuleShapeZ((btScalar)Radius, (btScalar)Height);
+	return G.addShape(shape);
+}
+
+DLLEXPORT MAB_ShapeCreatePlane(double NX, double NY, double NZ, double Distance)
+{
+	btCollisionShape* shape = new btStaticPlaneShape(btVector3((btScalar)NX, (btScalar)NY, (btScalar)NZ), (btScalar)Distance);
+	return G.addShape(shape);
+}
+
+DLLEXPORT MAB_ShapeCreateCylinder(double Radius, double Height, double UpAxis)
+{
+	int axis = (int)UpAxis;
+	btCollisionShape* shape;
+	if (axis == 0) shape = new btCylinderShapeX(btVector3((btScalar)Height, (btScalar)Radius, (btScalar)Radius));
+	else if (axis == 1) shape = new btCylinderShape(btVector3((btScalar)Radius, (btScalar)Height, (btScalar)Radius));
+	else shape = new btCylinderShapeZ(btVector3((btScalar)Radius, (btScalar)Radius, (btScalar)Height));
+	return G.addShape(shape);
+}
+
+DLLEXPORT MAB_ShapeCreateCone(double Radius, double Height, double UpAxis)
+{
+	int axis = (int)UpAxis;
+	btCollisionShape* shape;
+	if (axis == 0) shape = new btConeShapeX((btScalar)Radius, (btScalar)Height);
+	else if (axis == 1) shape = new btConeShape((btScalar)Radius, (btScalar)Height);
+	else shape = new btConeShapeZ((btScalar)Radius, (btScalar)Height);
+	return G.addShape(shape);
+}
+
 DLLEXPORT MAB_ShapeDestroy(double ShapeID)
 {
 	int ID = (int)ShapeID;
@@ -20,3 +56,5 @@ DLLEXPORT MAB_ShapeDestroy(double ShapeID)
 	G.Shapes.erase(ID);
 	return 1;
 }
+
+
---
+++ MAE - GML/MAE.gmx/scripts/MAE_PhysShapeCreateCone.gml
@@ -0,0 +1,3 @@
+///MAE_PhysShapeCreateCone(radius, height, axis)
+
+return external_call(global.MAB_ShapeCreateCone, argument0, argument1, argument2);
