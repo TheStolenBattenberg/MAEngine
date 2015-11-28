dab510045c9f4d075a423318d495beced79772ec
TickleForce <koderkk@gmail.com>
2015-11-27 22:21:20 -0500

    Added more physics constraint types
    Former-commit-id: c182be2b5280dd4070c9fa995980fdf07e64c16e

--- MAE - GML/DLLs/MA_Bullet/MA_Bullet/MABullet_Constraints.cpp
+++ MAE - GML/DLLs/MA_Bullet/MA_Bullet/MABullet_Constraints.cpp
@@ -1,5 +1,7 @@
 #include "MABullet.h"
 
+btTransform ConstraintTransformA, ConstraintTransformB;
+
 DLLEXPORT MAB_ConstraintCreateP2P(double BodyA, double AX, double AY, double AZ, double WorldA, double BodyB, double BX, double BY, double BZ, double WorldB)
 {
 	btPoint2PointConstraint* constraint;
@@ -24,13 +26,125 @@ DLLEXPORT MAB_ConstraintCreateFixed(double BodyA, double BodyB)
 	return G.addConstraint(constraint);
 }
 
+DLLEXPORT MAB_ConstraintCreateSlider(double BodyA, double BodyB, double LinearReferenceFrameA, double DisableCollision)
+{
+	btSliderConstraint* constraint;
+	btRigidBody* bodyA = G.getBody(BodyA);
+	if (BodyB > 0) {
+		btRigidBody* bodyB = G.getBody(BodyB);		
+		constraint = new btSliderConstraint(*bodyA, *bodyB, ConstraintTransformA, ConstraintTransformB, (LinearReferenceFrameA > 0));
+	}
+	else{
+		constraint = new btSliderConstraint(*bodyA, ConstraintTransformA, (LinearReferenceFrameA > 0));
+	}
+	G.World->addConstraint(constraint, (DisableCollision > 0));
+	return G.addConstraint(constraint);
+}
+
+DLLEXPORT MAB_ConstraintCreateGear(double BodyA, double BodyB, double AX, double AY, double AZ, double BX, double BY, double BZ, double Ratio, double DisableCollision)
+{
+	btGearConstraint* constraint;
+	btRigidBody* bodyA = G.getBody(BodyA);
+	btRigidBody* bodyB = G.getBody(BodyB);
+	btVector3 axisInA = btVector3((btScalar)AX, (btScalar)AY, (btScalar)AZ);
+	btVector3 axisInB = btVector3((btScalar)BX, (btScalar)BY, (btScalar)BZ);
+	constraint = new btGearConstraint(*bodyA, *bodyB, axisInA, axisInB, Ratio);
+	G.World->addConstraint(constraint, (DisableCollision > 0));
+	return G.addConstraint(constraint);
+}
+
+DLLEXPORT MAB_ConstraintCreateHinge(double BodyA, double BodyB, double UseReferenceFrameA, double DisableCollision)
+{
+	btHingeConstraint* constraint;
+	btRigidBody* bodyA = G.getBody(BodyA);
+	if (BodyB > 0) {
+		btRigidBody* bodyB = G.getBody(BodyB);
+		constraint = new btHingeConstraint(*bodyA, *bodyB, ConstraintTransformA, ConstraintTransformB, (UseReferenceFrameA > 0));
+	}
+	else {
+		constraint = new btHingeConstraint(*bodyA, ConstraintTransformA, (UseReferenceFrameA > 0));
+	}
+	G.World->addConstraint(constraint, (DisableCollision > 0));
+	return G.addConstraint(constraint);
+}
+
+DLLEXPORT MAB_ConstraintCreate6DOF(double BodyA, double BodyB, double UseReferenceFrameA, double DisableCollision)
+{
+	btGeneric6DofConstraint* constraint;
+	btRigidBody* bodyA = G.getBody(BodyA);
+	if (BodyB > 0) {
+		btRigidBody* bodyB = G.getBody(BodyB);
+		constraint = new btGeneric6DofConstraint(*bodyA, *bodyB, ConstraintTransformA, ConstraintTransformB, (UseReferenceFrameA > 0));
+	}
+	else {
+		constraint = new btGeneric6DofConstraint(*bodyA, ConstraintTransformA, (UseReferenceFrameA > 0));
+	}
+	G.World->addConstraint(constraint, (DisableCollision > 0));
+	return G.addConstraint(constraint);
+}
+
+DLLEXPORT MAB_ConstraintCreateSpring6DOF(double BodyA, double BodyB, double UseReferenceFrameA, double DisableCollision)
+{
+	btGeneric6DofSpringConstraint* constraint;
+	btRigidBody* bodyA = G.getBody(BodyA);
+	if (BodyB > 0) {
+		btRigidBody* bodyB = G.getBody(BodyB);
+		constraint = new btGeneric6DofSpringConstraint(*bodyA, *bodyB, ConstraintTransformA, ConstraintTransformB, (UseReferenceFrameA > 0));
+	}
+	else {
+		constraint = new btGeneric6DofSpringConstraint(*bodyA, ConstraintTransformA, (UseReferenceFrameA > 0));
+	}
+	G.World->addConstraint(constraint, (DisableCollision > 0));
+	return G.addConstraint(constraint);
+}
+
+DLLEXPORT MAB_ConstraintParamTransform(double n, double X, double Y, double Z, double RX, double RY, double RZ, double RW)
+{
+	btVector3 vec = btVector3((btScalar)X, (btScalar)Y, (btScalar)Z);
+	btQuaternion quat = btQuaternion((btScalar)RX, (btScalar)RY, (btScalar)RZ, (btScalar)RW);	
+	if(n == 0) ConstraintTransformA = btTransform(quat, vec);
+	else ConstraintTransformB = btTransform(quat, vec);
+	return 1;
+}
+
 DLLEXPORT MAB_ConstraintSetPivot(double ConstraintID, double n, double X, double Y, double Z)
 {
+	// Only works on P2P constraints
 	if (!G.constraintExists(ConstraintID)) return 0;
 	btPoint2PointConstraint* p2p = dynamic_cast<btPoint2PointConstraint*>(G.getConstraint(ConstraintID));
 	if (!p2p) return -1;
 	if (n > 0) p2p->setPivotB(btVector3((btScalar)X, (btScalar)Y, (btScalar)Z));
 	else p2p->setPivotA(btVector3((btScalar)X, (btScalar)Y, (btScalar)Z));
+	return 1;
+}
+
+DLLEXPORT MAB_ConstraintSetImpulseClamp(double ConstraintID, float amount)
+{
+	// Only works on P2P constraints
+	if (!G.constraintExists(ConstraintID)) return 0;
+	btPoint2PointConstraint* p2p = dynamic_cast<btPoint2PointConstraint*>(G.getConstraint(ConstraintID));
+	if (!p2p) return -1;
+	p2p->m_setting.m_impulseClamp = amount;
+	return 1;
+}
+
+DLLEXPORT MAB_ConstraintSetDamping(double ConstraintID, float amount)
+{
+	// Only works on P2P constraints
+	if (!G.constraintExists(ConstraintID)) return 0;
+	btPoint2PointConstraint* p2p = dynamic_cast<btPoint2PointConstraint*>(G.getConstraint(ConstraintID));
+	if (!p2p) return -1;
+	p2p->m_setting.m_damping = amount;
+	return 1;
+}
+
+DLLEXPORT MAB_ConstraintSetTau(double ConstraintID, float amount)
+{
+	// Only works on P2P constraints
+	if (!G.constraintExists(ConstraintID)) return 0;
+	btPoint2PointConstraint* p2p = dynamic_cast<btPoint2PointConstraint*>(G.getConstraint(ConstraintID));
+	if (!p2p) return -1;
+	p2p->m_setting.m_tau = amount;
 	return 1;
 }
 
