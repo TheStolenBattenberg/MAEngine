34a9381bffbc97ca34704738a4358ce1e4c554a0
TSB <mentisanimae@gmail.com>
2015-12-30 03:47:46 +0000

    More math
    Distance functions added, for if they're needed. I also made the cosine
    interpolation more precise by using 'M_PI' rather than a very
    non-precise '3.14'.

--- MAE - GML/DLLs/MA_DX9/MA_DX9/Math.cpp
+++ MAE - GML/DLLs/MA_DX9/MA_DX9/Math.cpp
@@ -18,7 +18,7 @@ float interpLinear(float x, float y, float s) {
 }
 
 float interpCosine(float x, float y, float s) {
-	float interp = (float)(1 - cos(s*3.14)) / 2;
+	float interp = (float)(1 - cos(s*M_PI)) / 2;
 	return (x * (1 - interp) + y*interp);
 }
 
@@ -30,4 +30,26 @@ float interpCubic(float x, float y, float z, float w, float s) {
 	float w1 = y;
 
 	return (x1*s*s1 + y1*s1 + z1*s + w1);
+}
+
+float distance1D(float x1, float x2) {
+	return x2 - x1;
+}
+
+//Would using pow(coord1, coord2) be faster? Probably not. Avoiding sqrt(value) would.
+float distance2D(float x1, float y1, float x2, float y2) { 
+	float xd, yd; 
+	xd = x2 - x1; 
+	yd = y2 - y1;
+
+	return (float)sqrt((xd*xd) + (yd*yd));
+}
+
+float distance3D(float x1, float y1, float z1, float x2, float y2, float z2) {
+	float xd, yd, zd;
+	xd = x2 - x1;
+	yd = y2 - y1;
+	zd = z2 - z1;
+
+	return (float)sqrt((xd*xd) + (yd*yd) + (zd*zd));
 }
--- MAE - GML/DLLs/MA_DX9/MA_DX9/Math.h
+++ MAE - GML/DLLs/MA_DX9/MA_DX9/Math.h
@@ -1,8 +1,9 @@
 #pragma once
+#define _USE_MATH_DEFINES
 
 #include "Types.h"
 #include <Windows.h>
-#include <math.h>
+#include <cmath>
 
 //Random
 float randomRangef(float min, float max);
@@ -12,3 +13,8 @@ uint  randomRange(uint min, uint max);
 float interpLinear(float x, float y, float s);
 float interpCosine(float x, float y, float s);
 float interpCubic(float x, float y, float z, float w, float s);
+
+//Distance
+float distance1D(float x1, float x2); //Pretty pointless.
+float distance2D(float x1, float y1, float x2, float y2);
+float distance3D(float x1, float y1, float z1, float x2, float y2, float z2);
