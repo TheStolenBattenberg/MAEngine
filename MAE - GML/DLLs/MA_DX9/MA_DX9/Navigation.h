2ebc231d6a4c804bb6381f7eeafd06a019f3759b
TickleForce <koderkk@gmail.com>
2016-01-02 19:35:03 -0500

    Worked on navmesh generation
    Former-commit-id: a4f5f109f35c5e9ba37619b68a0317babbb95be3

--- MAE - GML/DLLs/MA_DX9/MA_DX9/Navigation.h
+++ MAE - GML/DLLs/MA_DX9/MA_DX9/Navigation.h
@@ -1,18 +1,43 @@
 #pragma once
 
 #include "Recast.h"
+#include "DetourNavMesh.h"
+#include "DetourNavMeshBuilder.h"
+#include "DetourNavMeshQuery.h"
 
 struct MANavigation {
 	rcConfig m_cfg;
 
-	rcHeightfield* m_solid;
-	rcCompactHeightfield* m_chf;
-	rcContourSet* m_cset;
-	rcPolyMesh* m_pmesh;
-	rcPolyMeshDetail* m_dmesh;
 	rcContext* m_ctx;
+	dtNavMeshQuery* m_navQuery;
+
+	rcHeightfield* m_solid = nullptr;
+	rcCompactHeightfield* m_chf = nullptr;
+	rcContourSet* m_cset = nullptr;
+	rcPolyMesh* m_pmesh = nullptr;
+	rcPolyMeshDetail* m_dmesh = nullptr;	
+	dtNavMesh* m_navMesh = nullptr;	
+	unsigned char* m_triareas = nullptr;
+
+	float m_cellSize;
+	float m_cellHeight;
+	float m_agentHeight;
+	float m_agentRadius;
+	float m_agentMaxClimb;
+	float m_agentMaxSlope;
+	float m_regionMinSize;
+	float m_regionMergeSize;
+	float m_edgeMaxLen;
+	float m_edgeMaxError;
+	float m_vertsPerPoly;
+	float m_detailSampleDist;
+	float m_detailSampleMaxError;
 
 	MANavigation();
+	~MANavigation();
+	void cleanup();
+	int createNavMesh();
+	
 };
 
 extern MANavigation* manav;
